701a8ddbf2672ac63e5079aaff7758e3d83c6be7
Dark Shadow <155859542+dark-shadow-LK@users.noreply.github.com>
2024-08-24 20:55:42 +0530

    😈😈𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐝𝐨𝐰😈😈
    😈😈𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐝𝐨𝐰😈😈

--- src/plugin/fetch.js
+++ src/plugin/fetch.js
@@ -23,7 +23,7 @@ const fetchData = async (m, Matrix) => {
 
       const contentType = res.headers.get('content-type');
       if (!/text|json/.test(contentType)) {
-        await Matrix.sendMedia(m.from, url, 'file', '> Api Fetched From HANSAMAL-MD', m);
+        await Matrix.sendMedia(m.from, url, 'file', '> Api Fetched From DARK-SHADOW-MD', m);
         return;
       }
 
