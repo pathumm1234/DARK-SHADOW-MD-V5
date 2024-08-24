c3b432d25ca4cfeb6ec7669f88e1ab607b857dc4
Dark Shadow <155859542+dark-shadow-LK@users.noreply.github.com>
2024-08-24 21:01:47 +0530

    😈😈𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐝𝐨𝐰😈😈
    😈😈𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐝𝐨𝐰😈😈

--- src/plugin/gdrive.js
+++ src/plugin/gdrive.js
@@ -20,7 +20,7 @@ const gdriveDownload = async (m, Matrix) => {
 
       if (gdriveInfo && gdriveInfo.status && gdriveInfo.data) {
         const mediaUrl = gdriveInfo.data;
-        const caption = `> © Powered By HANSAMAL-MD`;
+        const caption = `> © Powered By DARK-SHADOW-MD`;
 
         // Inferring the file type based on the file extension
         const extension = mediaUrl.split('.').pop().toLowerCase();
